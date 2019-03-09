77ac22ee4481d9b7074baecbb056cc1881bc756b
Alex Rhinehart <rhinehart.alex05@gmail.com>
2019-03-09 00:49:54 -0500

    added html, css

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+
+
+//The Office Trivia
+
+//Create an array of objects. 10 questions/4 answer choices
+//Create a function that displays a question and the 4 answers. 
+//Create a timer variable
+//Create a correct answers variable
+//Create an incorrect answers variable
+//Create an unawsered variable
+//Create a start button
